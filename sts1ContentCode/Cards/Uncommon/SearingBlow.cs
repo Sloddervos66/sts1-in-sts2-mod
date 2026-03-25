dfb34f42a2790fc3aff786b7e050b588ead0f10d
Sloddervos66 <sloddervos66@gmail.com>
2026-03-25 23:58:34 +0100

    Added Searing Blow

---
+++ sts1ContentCode/Cards/Uncommon/SearingBlow.cs
@@ -0,0 +1,31 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace sts1Content.sts1ContentCode.Cards.Uncommon;
+
+[Pool(typeof(IroncladCardPool))]
+public sealed class SearingBlow() : CustomCard(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(12m, ValueProp.Move)];
+    public override int MaxUpgradeLevel => int.MaxValue;
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+        
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(4m);
+    }
+}
