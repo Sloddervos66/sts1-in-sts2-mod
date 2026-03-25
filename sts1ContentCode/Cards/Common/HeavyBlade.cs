82b4d02a587cc1ed1a8772ae1e10b10eaa5f4c08
Sloddervos66 <sloddervos66@gmail.com>
2026-03-25 22:32:30 +0100

    Init with Heavy Blade

---
+++ sts1ContentCode/Cards/Common/HeavyBlade.cs
@@ -0,0 +1,43 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace sts1Content.sts1ContentCode.Cards.Common;
+
+[Pool(typeof(IroncladCardPool))]
+public sealed class HeavyBlade() : CustomCard(2, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<StrengthPower>()];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new("Multiplier", 3m),
+        new CalculationBaseVar(14m),
+        new ExtraDamageVar(1m),
+        new CalculatedDamageVar(ValueProp.Move).WithMultiplier((card, _) =>
+        {
+            var strength = card.Owner.Creature.GetPowerAmount<StrengthPower>();
+            return strength * (card.DynamicVars["Multiplier"].BaseValue - 1);
+        })
+    ];
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+        
+        await DamageCmd.Attack(DynamicVars.CalculatedDamage).FromCard(this).Targeting(target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["Multiplier"].UpgradeValueBy(2m);
+    }
+}
---
+++ sts1ContentCode/Powers/sts1ContentPower.cs
@@ -0,0 +1,28 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using sts1Content.sts1ContentCode.Extensions;
+using Godot;
+
+namespace sts1Content.sts1ContentCode.Powers;
+
+public abstract class sts1ContentPower : CustomPowerModel
+{
+    //Loads from sts1Content/images/powers/your_power.png
+    public override string CustomPackedIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+            return ResourceLoader.Exists(path) ? path : "power.png".PowerImagePath();
+        }
+    }
+
+    public override string CustomBigIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
+            return ResourceLoader.Exists(path) ? path : "power.png".BigPowerImagePath();
+        }
+    }
+}
