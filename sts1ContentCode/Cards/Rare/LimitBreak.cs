cf7e747c72ff52965ff2997519506ff91677c88d
Sloddervos66 <sloddervos66@gmail.com>
2026-03-25 22:46:51 +0100

    Added Limit Break

---
+++ sts1ContentCode/Cards/Rare/LimitBreak.cs
@@ -0,0 +1,32 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace sts1Content.sts1ContentCode.Cards.Rare;
+
+[Pool(typeof(IroncladCardPool))]
+public sealed class LimitBreak() : CustomCard(1, CardType.Skill, CardRarity.Rare, TargetType.Self)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<StrengthPower>()];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        if (!Owner.Creature.HasPower<StrengthPower>()) return;
+        
+        var currentAmount = Owner.Creature.GetPowerAmount<StrengthPower>();
+        var powerModel = Owner.Creature.GetPower<StrengthPower>();
+        
+        await PowerCmd.ModifyAmount(powerModel!, currentAmount, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
+}
