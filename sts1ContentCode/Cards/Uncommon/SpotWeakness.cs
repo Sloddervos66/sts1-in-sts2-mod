a8298e350f577734b1e0d6aa471f37c2c9803d2d
Sloddervos66 <sloddervos66@gmail.com>
2026-03-25 23:51:27 +0100

    Added Spot Weakness

---
+++ sts1ContentCode/Cards/Uncommon/SpotWeakness.cs
@@ -0,0 +1,36 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace sts1Content.sts1ContentCode.Cards.Uncommon;
+
+[Pool(typeof(IroncladCardPool))]
+public sealed class SpotWeakness() : CustomCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<StrengthPower>()];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<StrengthPower>(3m)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        
+        var enemyIntendsToAttack = target.Monster?.IntendsToAttack ?? false;
+        if (enemyIntendsToAttack)
+        {
+            await PowerCmd.Apply<StrengthPower>(Owner.Creature, DynamicVars.Strength.BaseValue, Owner.Creature, this);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Strength.UpgradeValueBy(1m);
+    }
+}
